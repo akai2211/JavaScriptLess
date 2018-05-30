9ff9c7f605d9f66afb08e6f1be8720030dd6bd9b
Griroriy <wipmoon@gmail.com>
2018-05-31 01:36:43 +0700

    Повторное исправление сотрудников

--- Lesson_1/script.js
+++ Lesson_1/script.js
@@ -1,15 +1,12 @@
 /*домашее задание 1*/
 
-var budgetMonth = prompt("Ваш бюджет на месяц?");
-var nameShop = prompt("Название вашего магазина?");
+var budgetMonth = prompt("Ваш бюджет на месяц?"),
+		nameShop = prompt("Название вашего магазина?");
 
-var typeProduct1 = prompt("Какой тип товаров будем продавать?");
-var typeProduct2 = prompt("Какой тип товаров будем продавать?");
-var typeProduct3 = prompt("Какой тип товаров будем продавать?");
+var typeProduct1 = prompt("Какой тип товаров будем продавать?"),
+		typeProduct2 = prompt("Какой тип товаров будем продавать?"),
+		typeProduct3 = prompt("Какой тип товаров будем продавать?");
 
-var employers1 = prompt("Имя сотрудника:");
-var employers2 = prompt("Имя сотрудника:");
-var employers3 = prompt("Имя сотрудника:");
 
 /*var i = 0;
 while(i<3){
@@ -23,7 +20,11 @@ mainList = {
 	budget: budgetMonth,
 	nameShop: nameShop,
 	shopGoods: [typeProduct1, typeProduct2, typeProduct3],
-	employers: [employers1, employers2, employers3],
+	employers = {
+		name: "Михаил",
+		age: 24,
+		isMerriad: true
+	},
 	open: true
 };
 
