b7455210bb70e867ce553a22fc3a08443e04d274
Griroriy <wipmoon@gmail.com>
2018-06-27 23:34:43 +0700

    Подправил 12 14 дз из за calc

--- Lesson_12/js/script.js
+++ Lesson_12/js/script.js
@@ -223,53 +223,53 @@ window.addEventListener("DOMContentLoaded", () => {
 			});
 
 // Калькулятор
-let persons = document.getElementsByClassName("counter-block-input")[0],
-		restDays = document.getElementsByClassName("counter-block-input")[1],
-		place = document.getElementById("select"),
-		totalValue = document.getElementById("total"),
-		personsSum = 0,
-		daysSum = 0,
-		total = 0;
+	let persons = document.getElementsByClassName("counter-block-input")[0],
+			restDays = document.getElementsByClassName("counter-block-input")[1],
+			place = document.getElementById("select"),
+			totalValue = document.getElementById("total"),
+			personsSum = 0,
+			daysSum = 0,
+			total = 0;
 
-		totalValue.innerHTML = 0;
+			totalValue.innerHTML = 0;
 
-		persons.addEventListener("change", function () {
-			personsSum = +this.value;
-			total = (daysSum + personsSum) * 4000 * place.options[place.selectedIndex].value;;
-			if (restDays.value == "" || restDays.value == "0" || persons.value == "" || persons.value == "0") {
-				totalValue.innerHTML = 0;
-			} else {
-					totalValue.innerHTML = total;
-			}
-		});
-		restDays.addEventListener("change", function () {
-			daysSum = +this.value;
-			total = (daysSum + personsSum) * 4000 * place.options[place.selectedIndex].value;
-			if (persons.value == "" || persons.value == "0" || restDays.value == "" || restDays.value == "0") {
-				totalValue.innerHTML = 0;
-			} else {
-					totalValue.innerHTML = total;
-			}
-		});
+			persons.addEventListener("change", function () {
+				personsSum = +this.value;
+				total = (daysSum + personsSum) * 4000 * place.options[place.selectedIndex].value;;
+				if (restDays.value == "" || restDays.value == "0" || persons.value == "" || persons.value == "0") {
+					totalValue.innerHTML = 0;
+				} else {
+						totalValue.innerHTML = total;
+				}
+			});
+			restDays.addEventListener("change", function () {
+				daysSum = +this.value;
+				total = (daysSum + personsSum) * 4000 * place.options[place.selectedIndex].value;
+				if (persons.value == "" || persons.value == "0" || restDays.value == "" || restDays.value == "0") {
+					totalValue.innerHTML = 0;
+				} else {
+						totalValue.innerHTML = total;
+				}
+			});
 
-		place.addEventListener("change", function () {
-			if (restDays.value == "" || persons.value == "" || restDays.value == "0" || persons.value == "0") {
-				totalValue.innerHTML = 0;
-			} else {
-					let a = (daysSum + personsSum) * 4000;
-					totalValue.innerHTML = a * this.options[this.selectedIndex].value;
-			}
-		});
+			place.addEventListener("change", function () {
+				if (restDays.value == "" || persons.value == "" || restDays.value == "0" || persons.value == "0") {
+					totalValue.innerHTML = 0;
+				} else {
+						let a = (daysSum + personsSum) * 4000;
+						totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+				}
+			});
 
-		persons.onkeypress = (event) => {
-			if (event.key === "." || event.key === "," || event.key === "+" || event.key === '-' || event.key === 'e')
-				return false;
-			};
+			persons.onkeypress = (event) => {
+				if (event.key === "." || event.key === "," || event.key === "+" || event.key === '-' || event.key === 'e')
+					return false;
+				};
 
-		restDays.onkeypress = (event) => {
-			if (event.key === "." || event.key === "," || event.key === "+" || event.key === '-' || event.key === 'e')
-				return false;
-			};
+			restDays.onkeypress = (event) => {
+				if (event.key === "." || event.key === "," || event.key === "+" || event.key === '-' || event.key === 'e')
+					return false;
+				};
 
 
 
--- Lesson_14/js/parts/calc.js
+++ Lesson_14/js/parts/calc.js
@@ -1,5 +1,5 @@
 function calc() {
-	let persons = document.getElementsByClassName("counter-block-input")[0],
+let persons = document.getElementsByClassName("counter-block-input")[0],
 			restDays = document.getElementsByClassName("counter-block-input")[1],
 			place = document.getElementById("select"),
 			totalValue = document.getElementById("total"),
@@ -11,39 +11,39 @@ function calc() {
 
 			persons.addEventListener("change", function () {
 				personsSum = +this.value;
-				total = (daysSum + personsSum)*4000;
-				if (restDays.value == "") {
+				total = (daysSum + personsSum) * 4000 * place.options[place.selectedIndex].value;;
+				if (restDays.value == "" || restDays.value == "0" || persons.value == "" || persons.value == "0") {
 					totalValue.innerHTML = 0;
 				} else {
 						totalValue.innerHTML = total;
-				};
+				}
 			});
 			restDays.addEventListener("change", function () {
 				daysSum = +this.value;
-				total = (daysSum + personsSum)*4000;
-				if (persons.value == "") {
+				total = (daysSum + personsSum) * 4000 * place.options[place.selectedIndex].value;
+				if (persons.value == "" || persons.value == "0" || restDays.value == "" || restDays.value == "0") {
 					totalValue.innerHTML = 0;
 				} else {
 						totalValue.innerHTML = total;
-				};
+				}
 			});
 
 			place.addEventListener("change", function () {
-				if (restDays.value == "" || persons.value == "") {
+				if (restDays.value == "" || persons.value == "" || restDays.value == "0" || persons.value == "0") {
 					totalValue.innerHTML = 0;
 				} else {
-						let a = total;
+						let a = (daysSum + personsSum) * 4000;
 						totalValue.innerHTML = a * this.options[this.selectedIndex].value;
-				};
+				}
 			});
 
 			persons.onkeypress = (event) => {
-				if (event.key === "." || event.key === "+" || event.key === "e" || event.key === ',' || event.key === '-')
+				if (event.key === "." || event.key === "," || event.key === "+" || event.key === '-' || event.key === 'e')
 					return false;
 				};
 
 			restDays.onkeypress = (event) => {
-				if (event.key === "." || event.key === "," || event.key === "+" || event.key === 'e' || event.key === '-')
+				if (event.key === "." || event.key === "," || event.key === "+" || event.key === '-' || event.key === 'e')
 					return false;
 				};
 
